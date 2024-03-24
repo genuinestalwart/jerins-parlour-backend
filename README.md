a849c3a646e3aaf02d1718d6aaaf735f0c0d490e
genuinestalwart <genuinestalwart@gmail.com>
2024-03-24 15:49:29 +0600

    finished the backend side

---
+++ README.md
@@ -0,0 +1,3 @@
+# Jerins Parlour | Backend
+
+This is the server side code of this project. Checkout the [client side code](https://github.com/genuinestalwart/jerins-parlour-frontend) for more information.
--- index.ts
+++ index.ts
@@ -3,19 +3,15 @@ import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
 import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import Stripe from "stripe";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-
-const { clientEmail, privateKey, projectId } = JSON.parse(
-	process.env.FB_CREDENTIALS || ""
-);
-
-admin.initializeApp({
-	credential: admin.credential.cert({ clientEmail, privateKey, projectId }),
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+const credentials = JSON.parse(process.env.FB_CREDENTIALS!);
+admin.initializeApp({ credential: admin.credential.cert(credentials) });
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -38,6 +34,9 @@ app.get("/", (req, res) => {
 const run = async () => {
 	try {
 		// await client.connect();
+		const bookingsColl = client
+			.db("JerinsParlourDB")
+			.collection("bookings");
 		const reviewsColl = client.db("JerinsParlourDB").collection("reviews");
 		const servicesColl = client
 			.db("JerinsParlourDB")
@@ -158,6 +157,44 @@ const run = async () => {
 			async (req, res) => {
 				const _id = new ObjectId(req.params.id);
 				const result = await servicesColl.deleteOne({ _id });
+				res.send(result);
+			}
+		);
+
+		//
+
+		app.get("/bookings/:uid", verifyToken, async (req, res) => {
+			const { uid } = req.params;
+			const result = await bookingsColl.find({ uid }).toArray();
+			res.send(result);
+		});
+
+		app.get("/bookings", verifyToken, verifyAdmin, async (req, res) => {
+			const result = await bookingsColl.find().toArray();
+			res.send(result);
+		});
+
+		app.post("/create-payment-intent", verifyToken, async (req, res) => {
+			const amount = req.body.price * 100;
+			const payment_method_types = ["card"];
+			const intent = { amount, currency: "usd", payment_method_types };
+			const paymentIntent = await stripe.paymentIntents.create(intent);
+			res.send({ clientSecret: paymentIntent.client_secret });
+		});
+
+		app.post("/payments", verifyToken, async (req, res) => {
+			const result = await bookingsColl.insertOne(req.body);
+			res.send(result);
+		});
+
+		app.patch(
+			"/bookings/:id",
+			verifyToken,
+			verifyAdmin,
+			async (req, res) => {
+				const _id = new ObjectId(req.params.id);
+				const update = { $set: req.body };
+				const result = await bookingsColl.updateOne({ _id }, update);
 				res.send(result);
 			}
 		);
